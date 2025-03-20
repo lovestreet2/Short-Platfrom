a668b27e415a21c22aabc9485c70b04bf3e7c6d3
lovestreet202020 <lovestreet202020@gmail.com>
2025-03-20 21:55:20 +0530

    url changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -75,7 +75,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io("http://localhost:8000", {
+      const socketio = io("https://short-platfrom.onrender.com", {
         query: {
           userId: user?._id,
         },
